4da238401c7cb4fd1f846d9f0f35d1326914ccec
E-Link-U-Mission <278191463+E-LINK-U-Official@users.noreply.github.com>
2026-07-29 18:58:07 -0400

    Process and clean real estate rental data
    This script processes real estate rental data by cleaning column names, converting currency, and saving the cleaned data to a new table.

---
+++ pipeline_real_estate.py
@@ -0,0 +1,21 @@
+from pyspark.sql import functions as F
+
+# 1. Leer la tabla original
+df = spark.table("workspace.default.tabla_final_inmobiliaria")
+
+# 2. LIMPIEZA DE COLUMNA: Reemplazar el espacio por un guión bajo para cumplir con Delta
+df_limpio = df.withColumnRenamed("Renta Original", "Renta_Original")
+
+USD_TO_DOP_RATE = 59.50
+
+# 3. Aplicar la lógica usando el nuevo nombre de la columna (Renta_Original)
+df_procesado = df_limpio.withColumn(
+    "Renta_Calculada_DOP",
+    F.when(F.col("Moneda") == "USD", F.col("Renta_Original") * USD_TO_DOP_RATE)
+     .otherwise(F.col("Renta_Original"))
+)
+
+# 4. Mostrar los datos corregidos
+display(df_procesado)
+# Guardar la tabla sin errores de caracteres invalidos
+df_procesado.write.mode("overwrite").format("delta").saveAsTable("workspace.default.rentas_limpias_realestate")
